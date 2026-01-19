bc480144e6465ba465778717d6276c2313ea63e5
Jose-MonteFjr <zemfj11@gmail.com>
2026-01-19 16:44:34 -0300

    Objeto Math

---
+++ Fundamentos/objetoMath/math.js
@@ -0,0 +1,6 @@
+let num1 = 5.5689;
+console.log(Math.floor(num1)); // Math.floor arredonda para baixo(fixo) - 5
+console.log(Math.ceil(num1)); // Math.ceil arredonda para cima(fixo) - 6
+console.log(Math.round(num1)); // Math.round arredonda de forma dinâmica, arredonda para o valor mais próximo, ex - 1.2 = 1, 1.5 = 2 e etc
+console.log(Math.max(10, -1, 0, 500, 16, -89, 90, 72, 1, 2, 5, 8)); // Math.max Retorna o maior valor
+console.log(Math.min(10, -1, 0, 500, 16, -89, 90, 72, 1, 2, 5, 8)); // Math.min Retorna o menor valor
